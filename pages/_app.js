efa7d8b29ca01e08c747de5feb9dce626958cf9d
jamiebones <jamiebones2000@yahoo.co.uk>
2023-08-07 21:13:03 +0100

    fixed isssues

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,7 @@
 import "@/styles/globals.css";
 import Navbar from "../components/Navbar";
 import "flowbite";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://node1.bundlr.network/graphql/",
